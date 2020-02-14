40dff9ea55c8993af75cb55ff370cec5d90a8b81
Adokiye <gorgeceo@gmail.com>
2020-02-14 10:19:25 +0100

    Change of host name

--- root.js
+++ root.js
@@ -1,5 +1,5 @@
 
-export const API_URL= 'http://washnbox.com:9001/'         //          
+export const API_URL= 'http://washnbox.com:3001/'         //          
 export const APPLICATION_ID='sq0idp-ZWFqS07Li8TlWXwmwkZDoA'
 export const LOCATION_ID='PR3XJQ6YASSSN'
 // https://whispering-escarpment-09374.herokuapp.com/ | https://git.heroku.com/whispering-escarpment-09374.git
